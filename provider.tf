580844489451d36af6753cd69b81ec2d191fa36b
corry <github@corryh.com>
2022-11-05 13:24:06 -0700

    Remove backend, fix version

--- provider.tf
+++ provider.tf
@@ -4,11 +4,3 @@ provider "aws" {
   alias  = "us-east-1"
   region = "us-east-1"
 }
-
-terraform {
-  backend "s3" {
-    bucket = "net-corryh-terraform"
-    key    = "website/core.tf"
-    region = "us-west-2"
-  }
-}
